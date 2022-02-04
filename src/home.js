f6989f6ebd7ea7de29697fb804104f22f50fd789
Abhay <abhay.jha243@gmail.com>
2022-02-04 22:54:00 +0530

    Section 2 Done

--- src/home.js
+++ src/home.js
@@ -2,6 +2,8 @@ import './main.css'
 import logo from './images/rocket-img.png'
 import section1Image from './images/img.png'
 import wave1 from './images/wave-1.png'
+import zbclogo from './images/zbc-logo.png'
+import robot from './images/robot.png'
 
 function Home() {
   return (
@@ -25,6 +27,23 @@ function Home() {
       <img src={section1Image} alt="Image" className="section1Image" />
       </section>
       <img src={wave1} alt="Wave 1" className="homeWave" />
+    </section>
+
+    <section className="section2">
+      <section className="about1">
+        <img src={zbclogo} alt="Logo" className="zbclogo" />
+        <div className="about1Content">
+        <h1 className="aboutRight">About ZBC</h1>
+        <p className="aboutText">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eveniet quos perspiciatis consequatur at optio ipsum iusto quae cum id, laborum assumenda illo voluptas sint maiores blanditiis! Praesentium magnam rem qui.</p>
+        </div>
+      </section>
+      <section className="about2">
+        <div className="about2Content">
+        <h1 className="aboutLeft">About Space Division</h1>
+        <p className="aboutText2">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eveniet quos perspiciatis consequatur at optio ipsum iusto quae cum id, laborum assumenda illo voluptas sint maiores blanditiis! Praesentium magnam rem qui.</p>
+        </div>
+        <img src={robot} alt="Robot" className="robot" />
+      </section>
     </section>
     </>
   )
